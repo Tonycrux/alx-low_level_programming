7b7f3ece583366c3dedfd449dbbe47663f889038
Tonycrux <Sunnexbanty@gmail.com>
2021-06-17 00:35:42 -0700

    6th commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 /**
- * main - Prints ""Programming is like building a multilingual puzzle"
+ * main - Prints "Size of various types"
  * Return: 0
  */
 int main(void)
@@ -12,10 +12,10 @@ int main(void)
 	long longType;
 	long long longlongType;
 
-	printf("Size of a char: %zu bytes\n", sizeof(charType));
-	printf("Size of a int: %zu bytes\n", sizeof(intType));
-	printf("Size of a long int: %zu bytes\n", sizeof(longType));
-	printf("Size of a long long int: %zu bytes\n", sizeof(longlongType));
+	printf("Size of a char: %zu byte(s)\n", sizeof(charType));
+	printf("Size of a int: %zu byte(s)\n", sizeof(intType));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongType));
 	printf("Size of a float: %zu bytes\n", sizeof(floatType));
 	return (0);
 }
