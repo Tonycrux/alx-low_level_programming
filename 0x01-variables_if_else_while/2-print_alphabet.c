ab8827cadcb6fc161399b97c69941d2144775e42
Tonycrux <Sunnexbanty@gmail.com>
2021-06-18 12:56:24 -0700

    2nd commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Prints "Number is Positive, Zero or Negative"
+ * Return: 0
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	for (ch = 'a'; ch < 'z'; ch++)
+		putchar(ch);
+
+	return (0);
+}
