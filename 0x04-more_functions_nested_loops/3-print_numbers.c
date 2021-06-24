e7816024b2a3718a998b347d4f445ff74b07bf96
Tonycrux <Sunnexbanty@gmail.com>
2021-06-24 14:33:26 -0700

    3rd commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - For printing numbers
+ */
+
+void print_numbers(void)
+{
+	for (char i = 0; i < 9; i++)
+	{
+		_putchar (i);
+	}
+	_putchar ("\n");
+
+}
