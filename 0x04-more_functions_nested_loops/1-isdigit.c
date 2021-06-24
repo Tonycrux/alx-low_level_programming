1dc41e79471b2ab8f8725d151dc084c8b467959a
Tonycrux <Sunnexbanty@gmail.com>
2021-06-24 14:01:29 -0700

    1st commit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,11 +1,11 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
  * _isdigit - Check for number
  * @c: Character to check
  * Return: return 0 or 1.
  */
+
 int _isdigit(int c)
 {
 	return (c >= 48 && c <= 57);
