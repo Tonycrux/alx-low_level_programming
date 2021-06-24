cc6fa1bf14fcd24721c40a91b86595346fb312af
Tonycrux <Sunnexbanty@gmail.com>
2021-06-24 14:11:17 -0700

    2nd commit

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,21 +1,10 @@
 #include "holberton.h"
 
 /**
- * _isdigit - check for a digit
- * @c : character to check
- * Return:0 or 1
- */
-
-int _isdigit(int c)
-{
-	return (c >= 48 && c <= 57);
-
-}#include "holberton.h"
-
-/**
- * _isdigit - check for a digit
- * @c : character to check
- * Return:0 or 1
+ * mul - check for a digit
+ * @a : number to be multiplied
+ * @b : number to be multiplied
+ * Return: 0 or 1
  */
 
 int mul(int a, int b);
