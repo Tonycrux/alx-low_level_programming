0366927fec7fee83520657472556e1de4b535a9a
Tonycrux <Sunnexbanty@gmail.com>
2021-06-24 14:07:49 -0700

    2nd commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * _isdigit - check for a digit
+ * @c : character to check
+ * Return:0 or 1
+ */
+
+int _isdigit(int c)
+{
+	return (c >= 48 && c <= 57);
+
+}#include "holberton.h"
+
+/**
+ * _isdigit - check for a digit
+ * @c : character to check
+ * Return:0 or 1
+ */
+
+int mul(int a, int b);
+{
+	return (a * b);
+
+}
