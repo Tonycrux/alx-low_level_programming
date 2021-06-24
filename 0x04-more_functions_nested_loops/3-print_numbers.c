ec0a41bc5141bc3071dc9e3bb8ba01182f21bd47
Tonycrux <Sunnexbanty@gmail.com>
2021-06-24 14:37:54 -0700

    3rd commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,11 +2,12 @@
 
 /**
  * print_numbers - For printing numbers
+ * Return: 0
  */
 
 void print_numbers(void)
 {
-	for (char i = 0; i < 9; i++)
+	for (char i = 0; i <= 9; i++)
 	{
 		_putchar ('0' + n);
 	}
