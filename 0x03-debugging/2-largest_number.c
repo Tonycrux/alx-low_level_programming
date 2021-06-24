ce0eed94356e7196079667b4103ebf9e7e3bddcf
Tonycrux <Sunnexbanty@gmail.com>
2021-06-24 01:32:27 -0700

    wnd commit

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,3 +1,4 @@
+
 #include "holberton.h"
 
 /**
@@ -12,12 +13,12 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && b >= c)
+	if (a >= b && a >= c)
 	{
 		largest = a;
-	{
+	}
 
-	else if (b >= a && a >= c)
+	else if (b >= a && b >= c)
 	{
 		largest = b;
 	}
