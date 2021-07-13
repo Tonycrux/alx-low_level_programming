b59accf4ce629386d106beecda159dfd2fc9d034
Tonycrux <Sunnexbanty@gmail.com>
2021-07-12 21:25:41 -0700

    m

---
+++ 0x0A-argc_argv/holberton.h
@@ -0,0 +1,23 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+void _puts_recursion(char *);
+void _print_rev_recursion(char *);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *, char *);
+char *cap_string(char *);
+void _putchar(int);
+int _strlen(char *);
+int _strncmp(char *, char *, int );
+int _strlen_recursion(char *);
+int factorial(int n);
+int _pow_recursion(int , int );
+int _sqrt_recursion(int );
+int is_prime_number(int );
+int is_prime_number2(int , int );
+void myrecursion(char *, int );
+int count(char *, int );
+
+#endif /*HOLBERTON_H*/
