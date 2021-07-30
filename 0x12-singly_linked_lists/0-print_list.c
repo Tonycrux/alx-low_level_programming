3bda52573fc58430f17e33daa0789416f3fe4b43
Tonycrux <Sunnexbanty@gmail.com>
2021-07-29 21:28:29 -0700

    Commit commit

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,28 +1,22 @@
-#include "lists.h"
 #include <stdio.h>
+#include "lists.h"
 
 /**
- * print_list - Prints all the elements of a list_t list.
- * @h: The list_t list.
- *
- * Return: The number of nodes in h.
+ * print_list - prints all the elements of a list_t list
+ * @h: Listed lists
+ * Return: counter
  */
-
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	unsigned int count = 0;
 
 	while (h)
 	{
-		if (h->str == NULL)
+		count++;
+		h->str ? printf("[%d] %s\n", h->len, h->str) :
 			printf("[0] (nil)\n");
-
-		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		nodes++;
 		h = h->next;
 	}
 
-	return (nodes);
+	return (count);
 }
--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
+void __attribute__((constructor)) hare(void);
+
 /**
- * premain - run before main
+ * hare - Prints a string before the
+ *        main function is executed.
  */
-void __attribute__ ((constructor)) premain()
+void hare(void)
 {
-  printf(
-	 "You're beat! and yet, you must allow,\nI bore my house upon my back!\n"
-	 );
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
 }
